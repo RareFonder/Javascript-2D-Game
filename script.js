b16c6cd48e952a84682b3418cedd35a373582a59
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-09 20:22:30 +0800

    1 file modified

--- script.js
+++ script.js
@@ -39,6 +39,8 @@ window.onload = () => {
       this.frameX = 0;
       this.frameY = 0;
       this.speed = 0;
+      this.vy = 0;
+      this.weight = 0;
     }
     draw(context) {
       context.fillStyle = 'white';
@@ -46,16 +48,28 @@ window.onload = () => {
       context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
     }
     update(input) {
-      this.x += this.speed;
       if (input.keys.indexOf('ArrowRight') > -1 || input.keys.indexOf('d') > -1) {
         this.speed = 5;
       } else if (input.keys.indexOf('ArrowLeft') > -1 || input.keys.indexOf('a') > -1) {
         this.speed = -5;
+      } else if ((input.keys.indexOf('ArrowUp') > -1 || input.keys.indexOf('w') > -1) && this.onGround()) {
+        this.vy -= 20;
       } else {
         this.speed = 0;
       }
+      // Horizontal movement
+      this.x += this.speed;
       if (this.x < 0) this.x = 0;
       else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+
+      // Vertical movement
+      this.y += this.vy;
+      if (!this.onGround()) this.vy += this.weight;
+      else this.vy = 0;
+      if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
+    }
+    onGround() {
+      return this.y >= this.gameHeight - this.height;
     }
   };
 
