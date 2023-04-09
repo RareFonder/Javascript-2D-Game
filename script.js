c664d643f24007d83375e2eb2a26699a91a72bb5
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-09 20:37:19 +0800

    1 file modified

--- script.js
+++ script.js
@@ -87,9 +87,15 @@ window.onload = () => {
       this.y = 0;
       this.width = 2400;
       this.height = 720;
+      this.speed = 7;
     }
     draw(context) {
-      context.drawImage(this.image, this.x, this.y)
+      context.drawImage(this.image, this.x, this.y, this.width, this.height);
+      context.drawImage(this.image, this.x + this.width - this.speed, this.y, this.width, this.height);
+    }
+    update() {
+      this.x -= this.speed;
+      if (this.x < 0 - this.width) this.x = 0;
     }
   };
 
@@ -112,6 +118,7 @@ window.onload = () => {
   const animate = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     background.draw(ctx);
+    background.update();
     player.draw(ctx);
     player.update(input);
     requestAnimationFrame(animate);
