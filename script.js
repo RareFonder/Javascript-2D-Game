b96fea8163caf1a8a0ed0798b7e094974ec0b09e
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-09 19:01:36 +0800

    3 files modified

--- script.js
+++ script.js
@@ -1,4 +1,55 @@
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
-canvas.width = 500;
-canvas.height = 700;
+window.onload = () => {
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 800; 
+  canvas.height = 720;
+
+  class InputHandler {
+    constructor() {
+      this.keys = [];
+      window.addEventListener('keydown', e => {
+        if (( e.key === 'ArrowDown' || 
+              e.key === 'ArrowUp' || 
+              e.key === 'ArrowLeft' || 
+              e.key === 'ArrowRight') 
+              && this.keys.indexOf(e.key) === -1) {
+          this.keys.push(e.key);
+        }
+      });
+      window.addEventListener('keyup', e => {
+        if (  e.key === 'ArrowDown' || 
+              e.key === 'ArrowUp' || 
+              e.key === 'ArrowLeft' || 
+              e.key === 'ArrowRight') {
+          this.keys.splice(this.keys.indexOf(e.key), 1);
+        }
+      });
+    }
+  };
+
+  class Player {
+
+  };
+
+  class Background {
+
+  };
+
+  class Enemy {
+
+  };
+
+  const handleEnemies = () => {
+
+  };
+
+  const displayStatusText = () => {
+
+  };
+
+  const input = new InputHandler();
+
+  const animate = () => {
+
+  }
+};
